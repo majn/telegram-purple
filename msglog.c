a0b873933a75c68b43da02f96cb6d5237eb7e4ff
mjentsch <mtthsjntsch@gmail.com>
2015-03-12 01:39:03 +0100

    Remove colors from messagelog

--- msglog.c
+++ msglog.c
@@ -22,27 +22,11 @@
 #include <debug.h>
 #include "telegram-purple.h"
 
-#ifndef __ADIUM_
-#define COLOR_GREY "\033[37;1m"
-#define COLOR_YELLOW "\033[33;1m"
-#define COLOR_RED "\033[0;31m"
-#define COLOR_REDB "\033[1;31m"
-#define COLOR_GREEN "\033[32;1m"
-#define COLOR_NORMAL "\033[0m"
-#else
-#define COLOR_GREY ""
-#define COLOR_YELLOW ""
-#define COLOR_RED ""
-#define COLOR_REDB ""
-#define COLOR_GREEN ""
-#define COLOR_NORMAL ""
-#endif
-
 /*
   msglog.c: Convenience methods for logging to libpurple log
  */
 
-void log_level_printf (const char* format, va_list ap, int level, char *color) {
+void log_level_printf (const char* format, va_list ap, int level) {
   char buffer[256];
   vsnprintf (buffer, sizeof(buffer), format, ap);
   
@@ -50,41 +34,41 @@ void log_level_printf (const char* format, va_list ap, int level, char *color) {
   if (last >= 2 && buffer[last] == '\n') {
     buffer[last] = '\0';
   }
-  purple_debug (level, PLUGIN_ID, "%s%s%s\n", color, buffer, COLOR_NORMAL);
+  purple_debug (level, PLUGIN_ID, "%s\n", buffer);
 }
 
 void debug(const char* format, ...) {
   va_list ap;
   va_start (ap, format);
-  log_level_printf (format, ap, PURPLE_DEBUG_MISC, COLOR_NORMAL);
+  log_level_printf (format, ap, PURPLE_DEBUG_MISC);
   va_end (ap);
 }
 
 void info(const char* format, ...) {
   va_list ap;
   va_start (ap, format);
-  log_level_printf (format, ap, PURPLE_DEBUG_INFO, COLOR_GREEN);
+  log_level_printf (format, ap, PURPLE_DEBUG_INFO);
   va_end (ap);
 }
 
 void warning(const char* format, ...) {
   va_list ap;
   va_start (ap, format);
-  log_level_printf (format, ap, PURPLE_DEBUG_WARNING, COLOR_YELLOW);
+  log_level_printf (format, ap, PURPLE_DEBUG_WARNING);
   va_end (ap);
 }
 
 void failure(const char* format, ...) {
   va_list ap;
   va_start (ap, format);
-  log_level_printf (format, ap, PURPLE_DEBUG_ERROR, COLOR_YELLOW);
+  log_level_printf (format, ap, PURPLE_DEBUG_ERROR);
   va_end (ap);
 }
 
 void fatal(const char* format, ...) {
   va_list ap;
   va_start (ap, format);
-  log_level_printf (format, ap, PURPLE_DEBUG_FATAL, COLOR_REDB);
+  log_level_printf (format, ap, PURPLE_DEBUG_FATAL);
   va_end (ap);
   info ("\n");
 }
