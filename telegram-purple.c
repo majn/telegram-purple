e82fef382658e822986d0d809689aea37b158933
mjentsch <mtthsjntsch@gmail.com>
2015-09-08 22:09:15 +0200

    Prevent flood warnings
    Only load user info on first start when the user actually has a pic to display.

--- telegram-purple.c
+++ telegram-purple.c
@@ -331,9 +331,11 @@ static void on_get_dialog_list_done (struct tgl_state *TLS, void *callback_extra
       if (! buddy) {
         buddy = p2tgl_buddy_new (TLS, UC);
         purple_blist_add_buddy (buddy, NULL, tggroup, NULL);
-      }
-      if (UC->user.photo_id) {
-        tgl_do_get_user_info (TLS, UC->id, 0, on_user_get_info, get_user_info_data_new (0, UC->id));
+        
+        if (UC->user.photo_id) {
+          debug ("tgl_do_get_user_info(%s)", UC->print_name);
+          tgl_do_get_user_info (TLS, UC->id, 0, on_user_get_info, get_user_info_data_new (0, UC->id));
+        }
       }
       
       p2tgl_prpl_got_user_status (TLS, UC->id, &UC->user.status);
