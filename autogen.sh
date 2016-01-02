e5adfc1bd6d97e90dc8e2553ec1b6d4f953f2cc8
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-01-02 03:23:14 +0100

    Let autogen.sh take care of the .nsi

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
+set -e
+
 autoreconf
 
-echo bootstrapping translation files ...
+(
 cd po
 export XGETTEXT="xgettext -kP_:1,2"
 intltool-update --pot
 ## Translations are managed at https://www.transifex.com/telegram-purple-developers/telegram-purple/
 ## To update the .po files, download it from there, since intltool, msginit, and transifex produce slightly different files, and I'd like to avoid gigantic git diffs that only change indentation or similar things.
-#for lang in $(cat LINGUAS); do
-#    if [ -e "$lang.po" ]
-#    then
-#        echo "updating language file $lang.po ..."
-#        intltool-update "$lang"
-#    else
-#        echo "creating new language file $lang.po ..."
-#        msginit --locale="$lang"
-#    fi
-#done
-cd ..
+)
+
+test -r Makefile || (echo "Autoreconf didn't create Makefile?!"; exit 1)
+make --quiet build-nsi
