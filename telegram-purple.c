dd349ff7cbb6d5d3fedc90a08f62847d2c0aec5d
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-01-09 13:25:45 +0100

    Fix typos and errors in strings

--- telegram-purple.c
+++ telegram-purple.c
@@ -531,7 +531,7 @@ static void tgprpl_login (PurpleAccount * acct) {
       tgl_set_rsa_key_direct (TLS, pubkey.e, pubkey.n_len, pubkey.n_raw);
     } else {
       failure ("both didn't work. abort.");
-      char *cause = g_strdup_printf (_("Unable to sign on as %s: pubkey not found."),
+      char *cause = g_strdup_printf (_("Unable to sign on as %s: file (public key) not found."),
                       purple_account_get_username (acct));
       purple_connection_error_reason (gc, PURPLE_CONNECTION_ERROR_INVALID_SETTINGS, cause);
       char *long_hint = g_strdup_printf (
@@ -557,10 +557,11 @@ static void tgprpl_login (PurpleAccount * acct) {
   tgl_init (TLS);
 
   if (! tgp_startswith (purple_account_get_username (acct), "+")) {
-        char *cause = g_strdup_printf (_("Unable to sign on as %s: phone number lacks country prefix."
-                                         " Numbers must start with the full international"
-                                         " prefix code, e.g. +49 for Germany."),
-                        purple_account_get_username (acct));
+    // TRANSLATORS: Please fill in your own prefix!
+    char *cause = g_strdup_printf (_("Unable to sign on as %s: phone number lacks country prefix."
+                                     " Numbers must start with the full international"
+                                     " prefix code, e.g. +1 for USA."),
+                                   purple_account_get_username (acct));
     purple_connection_error_reason (gc, PURPLE_CONNECTION_ERROR_INVALID_SETTINGS, cause);
     purple_notify_message (_telegram_protocol, PURPLE_NOTIFY_MSG_ERROR, _("Incomplete phone number"),
         cause, NULL, NULL, NULL);
