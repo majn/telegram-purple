27469eec1826c9593702150c8dfbef992541ab5d
Maato <maato@softwarebakery.com>
2016-04-09 13:42:13 +0200

    Fix failure when trying to move downloaded file across partitions.

--- tgp-ft.c
+++ tgp-ft.c
@@ -19,6 +19,7 @@
  */
 
 #include <glib/gstdio.h>
+#include <gio/gio.h>
 
 #include "telegram-purple.h"
 
@@ -62,7 +63,10 @@ static char *tgp_strdup_determine_filename (const char *mime, const char *captio
 static void tgprpl_xfer_recv_on_finished (struct tgl_state *TLS, void *_data, int success, const char *filename) {
   debug ("tgprpl_xfer_recv_on_finished()");
   struct tgp_xfer_send_data *data = _data;
+
   char *selected = g_strdup (purple_xfer_get_local_filename (data->xfer));
+  GFile *source_file = g_file_new_for_path(filename);
+  GFile *destination_file = g_file_new_for_path(selected);
 
   if (success) {
     debug ("purple_xfer_set_completed");
@@ -89,9 +93,11 @@ static void tgprpl_xfer_recv_on_finished (struct tgl_state *TLS, void *_data, in
   tgprpl_xfer_free_data (data);
 
   debug ("moving transferred file from tgl directory %s to selected target %s", selected, filename);
-  g_unlink (selected);
-  g_rename (filename, selected);
-  g_free (selected);
+  g_file_move (source_file, destination_file, G_FILE_COPY_OVERWRITE,
+    NULL, NULL, NULL, NULL);
+  g_object_unref(destination_file);
+  g_object_unref(source_file);
+  g_free(selected);
 }
 
 static void tgprpl_xfer_send_on_finished (struct tgl_state *TLS, void *_data, int success, struct tgl_message *M) {
